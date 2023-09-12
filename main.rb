9f43c8b5329e20d4d88021d6024e3c9c4b9964f2
Yuki Matsukura <matsubokkuri@gmail.com>
2023-09-12 15:39:42 +0900

    upgraded gems and fixed logic.

--- main.rb
+++ main.rb
@@ -1,85 +1,72 @@
-require 'cosmicrawler'
-require 'nokogiri'
-require 'uri'
+require 'bundler'
+Bundler.require(:default)
+
 require 'logger'
 require 'open-uri'
-
 
 file = ARGV[0] ||= 'url.txt'
 
 urls = File.open(file).read.split("\n")
 
-
 # hash for result
 result = {}
 
-
 # email regex
-r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)     
-
+r = Regexp.new(/\b[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}\b/)
 
 logger = Logger.new(STDOUT)
 
+Cosmicrawler.http_crawl(urls, 16) do |request|
+  uri = request.uri
+  logger.info(uri)
 
+  # init result hash
+  result[uri] = []
 
-Cosmicrawler.http_crawl(urls, 16) {|request|
+  response = request.get.response
 
-  begin
-    uri = request.uri
-    logger.info(uri)
+  # find next pages
+  next_page_url = []
+  doc = Nokogiri::HTML(response)
+  doc.css('a').each do |anchor|
+    path = anchor[:href]
+    next unless path
+    next if path.match(/mailto/)
+    next if path.match(/\.pdf\Z/)
 
-    # init result hash
-    result[uri] = []
-
-    response = request.get.response
-
-
-    # find next pages
-    next_page_url = []
-    doc = Nokogiri::HTML(response)
-    doc.css('a').each do |anchor|
-      path =  anchor[:href]
-      next unless path
-      next if path.match(/mailto/)
-      next if path.match(/\.pdf\Z/)
-      next_page_url <<  URI.join(uri, path).to_s
-    end
-
-    # get email address
-    emails = response.scan(r).uniq
-
-    result[uri].concat(emails)
-    logger.debug(emails)
-
-    next_page_url.uniq!
-
-    return unless next_page_url
-    #
-    next_page_url.each do |url|
-
-      logger.info(url)
-
-      begin
-        response = open(url, { read_timeout: 5 }).read
-      rescue => e
-        logger.error(e)
-        next
-      end
-
-      emails = response.scan(r).uniq
-      result[uri].concat(emails)
-
-      logger.debug(emails)
-
-    end
-
-  rescue => e
-    logger.error(e)
+    next_page_url << URI.join(uri, path).to_s
   end
 
-}
+  # get email address
+  emails = response.scan(r).uniq
 
+  result[uri].concat(emails)
+  logger.debug(emails)
 
+  next_page_url.uniq!
+
+  return unless next_page_url
+
+  debugger
+
+  next_page_url.each do |url|
+    logger.info(url)
+
+    begin
+      response = URI.open(url, { read_timeout: 5 }).read
+    rescue StandardError => e
+      logger.error(e)
+      next
+    end
+
+    emails = response.scan(r).uniq
+    result[uri].concat(emails)
+
+    logger.debug(emails)
+  end
+rescue StandardError => e
+  logger.error(e)
+end
 
 result.each do |key, value|
   print key
@@ -87,4 +74,3 @@ result.each do |key, value|
   print value.uniq.join("\t")
   print "\n"
 end
-
